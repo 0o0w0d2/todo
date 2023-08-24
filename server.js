a7619a87124b888e6f27c081acf5764158792e8f
zion <0o0w0d2@gmail.com>
2023-08-25 01:49:01 +0900

    <form> 태그의 속성 action과 method를 통해 input을 서버로 보내기

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 
+app.use(express.urlencoded({ extended: true }));
+
 dotenv.config();
 
 const port = process.env.PORT;
@@ -15,4 +17,15 @@ app.get("/", (req, res) => {
 
 app.get("/write", (req, res) => {
   res.sendFile(__dirname + "/write.html");
+});
+
+app.post("/add", (req, res) => {
+  res.send("전송 완료!");
+  console.log(req.body.todo);
+  console.log(req.body.date);
+  if (!req.body.important) {
+    console.log("중요하지 않음!");
+  } else {
+    console.log("중요!");
+  }
 });
